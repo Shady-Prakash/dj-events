dca1ddfdff15651b9816195ca5cc0b1d6c8efbaf
Prakash Mahat <shadyprakash8@gmail.com>
2021-07-01 21:00:21 +0545

    head tags-layout

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,10 @@
 /** @format */
+import Layout from '../../components/Layout';
 
 export default function EventsPage() {
 	return (
-		<div>
+		<Layout>
 			<h1>My Events</h1>
-		</div>
+		</Layout>
 	);
 }
